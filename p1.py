3d269dac766f31682973c946711d40c3807da11c
Juan Holguin <juholguin@unal.edu.co>
2022-03-14 15:02:08 -0500

    respuesta 1.1

--- p1.py
+++ p1.py
@@ -1,3 +1,9 @@
-def pregunta01 ():
-    return 0
-
+import os
+with open("/home/juanes/Documents/UNAL/Ciencia_Datos/LAB/lab1/data.csv", "r") as file:
+        data = file.readlines()
+data = [line.replace("\n", "") for line in data]
+data = [line.split("\t") for line in data]
+suma = 0
+for x in data:
+    suma =  int(x[1]) + suma
+print (suma)
