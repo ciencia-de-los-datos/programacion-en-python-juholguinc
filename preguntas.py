57ee01c6d494f4c480324e3a08c4890b3cf3fe93
Juan Holguin <juholguin@unal.edu.co>
2022-03-15 09:51:55 -0500

    respuesta 1

--- preguntas.py
+++ preguntas.py
@@ -21,7 +21,17 @@ def pregunta_01():
     214
 
     """
-    return
+    with open("/home/juanes/Documents/UNAL/Ciencia_Datos/LAB/lab1/data.csv", "r") as file:
+        data = file.readlines()
+
+
+    data = [line.replace("\n", "") for line in data]
+    data = [line.split("\t") for line in data]
+    suma = 0
+    for x in data:
+        suma = int(x[1]) + suma
+    print(suma)
+    return suma
 
 
 def pregunta_02():
