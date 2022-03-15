d6d82dc18577ee5e27ecf83a27622fb5b2569da4
Juan Holguin <juholguin@unal.edu.co>
2022-03-15 08:58:06 -0500

    respuestas 7-8-9-10

---
+++ p8.py
@@ -0,0 +1,55 @@
+import os
+with open("/home/juanes/Documents/UNAL/Ciencia_Datos/LAB/lab1/data.csv", "r") as file:
+        data = file.readlines()
+data = [line.replace("\n", "") for line in data]
+data = [line.split("\t") for line in data]
+mapl = []
+for value in data:
+    tup1 = value
+    int1 = int(tup1 [1])
+    tupa = int1,tup1[0]
+    mapl.append(tupa)
+    mapl.sort(reverse=False)
+print (mapl)
+def unique(list1):
+    unique_list = []
+    for x in list1:
+        if x not in unique_list:
+            unique_list.append(x)
+    return unique_list
+    # print list
+def reducer(sequence):
+    x = 0
+    reduce1 = []
+    while x < len(sequence):
+    #for x in range(len(mapl)):
+        #print (x)
+        tup1 = sequence[x]
+        key1 = tup1[0]
+        count = []
+        count.append(tup1[1])
+        #print (count)
+        y = x
+        bolx = True
+        while (bolx == True):
+            #print(key1)
+            if (x+1 == len(sequence)):
+                x = x+1
+                break
+            tup2 = sequence[x+1]
+            key2 = tup2[0]
+            if key1 == key2:
+                count.append(tup2[1])
+                #rint (count)
+                x = x+1
+            else:
+                bolx = False
+                x = x+1
+
+        tup = tuple()
+        ulist = unique(count)
+        tup = key1, ulist
+        reduce1.append(tup)
+    return reduce1
+result = reducer(mapl)
+print (result)
