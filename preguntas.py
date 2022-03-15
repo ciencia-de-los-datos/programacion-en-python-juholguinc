45f896474f11881615d8606ffc2ab6ca055091dd
Juan Holguin <juholguin@unal.edu.co>
2022-03-15 13:13:27 -0500

    respuesta 1

--- preguntas.py
+++ preguntas.py
@@ -12,7 +12,7 @@ Utilice el archivo `data.csv` para resolver las preguntas.
 
 """
 global data
-with open("/home/juanes/Documents/UNAL/Ciencia_Datos/LAB/lab1/data.csv", "r") as file:
+with open("data.csv", "r") as file:
     data = file.readlines()
 data = [line.replace("\n", "") for line in data]
 data = [line.split("\t") for line in data]
